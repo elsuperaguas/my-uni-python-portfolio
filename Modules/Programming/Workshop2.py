7828d974616589b24cf6be138777525689bf244c
Camilo <136385165+elsuperaguas@users.noreply.github.com>
2025-11-13 15:53:27 +0000

    Create Workshop2.py with multiple programming tasks
    Added a structured Python program with multiple tasks demonstrating basic programming concepts.

---
+++ Modules/Programming/Workshop2.py
@@ -0,0 +1,30 @@
+task1 = "task1:"
+print(task1,"Welcome to the world of programming!")
+
+task2 = "task2:"
+#structured_intro.py.defines a function and then calls it.
+def main():
+    print(task2,"This program is now properly structured")
+main()
+
+task3 = "task3:"
+def main():
+    print(task3,"There once was a computer that learned to code.")
+    print('It started with a simple "Hello, World!"')
+    print("Soon, it was building programs of its own")
+main()
+
+task4 = "task4:"
+def main():
+    program_name = "Chaos Illustrator"
+    print(task4,"the name of this programe is")
+    print(program_name)
+main()
+
+task5 = "task5:"
+#this program will perform a simple calculation.
+def main():
+    x = 0.5
+    result = 2.0 * x * (1 - x)
+    print(task5,result)
+main()
