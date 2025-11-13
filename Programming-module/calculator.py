afb0ccaa7f5a6c5d662f7690af2c510a62742523
Camilo <136385165+elsuperaguas@users.noreply.github.com>
2025-11-13 15:43:44 +0000

    Delete Programming-module directory

--- Programming-module/calculator.py
+++
@@ -1,6 +0,0 @@
-def calculator():
-    for i in range(3):
-        expression = input("Enter a mathematical expression: ")
-        result = eval(expression)
-        print(f"The result is: {result}")
-calculator()
